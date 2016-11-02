a200f84418d6b950a30b7c84b92c369bc03a734d
Bayu Candra <bayucandra@gmail.com>
2016-11-02 10:04:08 +0700

    Add line strokeWidth maintainable.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
-
+var OBIQApp=null;//global variable
+var OBFabricPolygon =null; //stand as global variable
 $(document).ready(function(){
-    var OBIQApp = new BIQApp;
+    OBIQApp = new BIQApp;
     OBIQApp.init();
-    var OBFabricPolygon = new BFabricPolygon({ canvas : OBIQApp.canvas });
+    OBFabricPolygon = new BFabricPolygon({ canvas : OBIQApp.canvas });
+    
+    OBFabricPolygon.createBRect();
 });
 function BIQApp(){
 }
@@ -12,7 +15,7 @@ BIQApp.prototype.init = function(){
         id: "osd1",
         prefixUrl: "assets/js/osd/images/",
         debugMode:false,
-        tileSources:'assets/dzi/magickslicer/A1.0.dzi',
+        tileSources:'assets/dzi/magickslicer/home-schema2.dzi',
         visibilityRatio:1.0,
         constrainDuringPan: true,
         animationTime: self.animationTime,
@@ -20,12 +23,43 @@ BIQApp.prototype.init = function(){
         maxImageCacheCount: 2000,
         preserveViewport:true
     });
+    self.isMaintainFabricSizes = true;
+    $('.maintain_line_check').bootstrapToggle('on');
     var overlay = self.viewer.fabricjsOverlay();
-    self.canvas= self.viewer.fabricjsOverlay().fabricCanvas();
+    self.canvas = self.viewer.fabricjsOverlay().fabricCanvas();
     $(window).resize(function() {
         overlay.resize();
         overlay.resizecanvas();
     });
+    
+    self.zoomRatio = self.viewer.viewport.getZoom() / self.viewer.viewport.imageToViewportZoom(1);
+    
+    self.viewer.addHandler('zoom', function(event){//update self.zoomRatio and OBFabOBFabricPolygon.viewerRatio on zoom
+        self.zoomRatio = event.eventSource.viewport.getZoom() / self.viewer.viewport.imageToViewportZoom(1);
+//        console.log(event.eventSource.viewport.getZoom());
+        
+        if(self.isMaintainFabricSizes){
+            OBFabricPolygon.viewerRatio = self.zoomRatio;
+        }
+    });
+};
+BIQApp.prototype.zoomActual = function(){
+    var self = this;
+    self.viewer.viewport.zoomTo(self.viewer.viewport.imageToViewportZoom(1));
+};
+BIQApp.prototype.maintainFabricSizes = function( event ){
+    if( OBFabricPolygon===null ){
+        return;
+    }
+    var self = this;
+    self.isMaintainFabricSizes = $(event).prop('checked');
+    if(self.isMaintainFabricSizes){
+        OBFabricPolygon.viewerRatio = self.zoomRatio;
+    }else{
+        self.zoomRatio = 1;
+        if(OBFabricPolygon!==null)OBFabricPolygon.viewerRatio = 1;
+    }
+    
 };
 
 //var prototypefabric = new function () {
--- assets/js/bfabric-polygon.js
+++ assets/js/bfabric-polygon.js
@@ -10,17 +10,26 @@ var BFabricPolygon = (function(){
 
         self.min = 99;
         self.max = 999999;
-        self.polygonMode = true;
+        self.polygonMode = false;
         self.pointArray = new Array();
         self.lineArray = new Array();
         self.activeLine = null;
         self.activeShape = false;
         self.canvas = options.canvas;
+        
+        self.viewerRatio = 1;//This ratio will be multiplied by some number to get forexample strokeWidth or circle radius etc.
 
+        self.BLineInit(self);
+        self.BRectInit(self);
+        
         self.strokeWidthLine = 50;
         self.strokeWidthCircle = 10;
         self.circlePointRadius = 50;
 
+        if(jQuery && jQuery().notify){
+            var notify_defaults = { autoHideDelay: 2000, globalPosition: 'bottom left' };
+            jQuery.notify.defaults(notify_defaults);
+        }
 
         self.canvas.on('mouse:down', function ( event ) {
             if( event.target && event.target.id === self.pointArray[0].id ){// if back to starting point=========
@@ -29,14 +38,16 @@ var BFabricPolygon = (function(){
                 self.addPoint( event );
             }
         });
+        
         self.canvas.on('mouse:up', function (options) {
 
         });
+        
         self.canvas.on('mouse:move', function (options) {
             if(self.activeLine === null){return;}
     //            console.log(prototypefabric.activeLine);
     //            console.log(prototypefabric.activeLine.class);
-            if(self.activeLine && self.activeLine.class == "line"){
+            if(self.activeLine && self.activeLine.get('type') === "BLine"){
                 var pointer = self.canvas.getPointer(options.e);
                 self.activeLine.set({ x2: pointer.x, y2: pointer.y });//move the 2 point of active line based on cursor position
 
@@ -53,12 +64,71 @@ var BFabricPolygon = (function(){
             self.canvas.renderAll();
         });
     };
+    BFabricPolygon.prototype.BLineInit = function(self){
+        self.BLine = fabric.util.createClass(fabric.Line, {
+            type:'BLine', strokeWidthBase : 10,
+            setStroke: function(){
+                var self2 = this;
+                var strokeWidth = self2.strokeWidthBase / self.viewerRatio;
+//                console.log(self2.strokeWidthBase.toString()+' '+self.viewerRatio.toString()+' '+strokeWidth.toString());
+                self2.set( { strokeWidth: strokeWidth } );
+            },
+            initialize:function(options, properties){
+		options || (options={ });
+                var self2 = this;
+                self2.callSuper('initialize', options);
+                self2.setStroke();
+                self2.set(properties);
+            },
+            _render: function(ctx){
+                var self2 = this;
+                self2.callSuper('_render', ctx);
+                self2.setStroke();
+            }
+        });
+    };
+    BFabricPolygon.prototype.BRectInit = function(self){
+        self.BRect = fabric.util.createClass(fabric.Rect, {
+            type:'BRect', widthBase : 500, heightBase: 100, leftBase: 300, topBase:200,
+            bSetSizes: function(){
+                var self2 = this;
+                var top = self2.topBase / self.viewerRatio;
+                var left = self2.leftBase / self.viewerRatio;
+                var width = self2.widthBase / self.viewerRatio;
+                var height = self2.heightBase / self.viewerRatio;
+                var prop = { top:top, left:left, width:width, height:height };
+                self2.set(prop);
+            },
+            initialize:function(options){
+		options || (options={ });
+                var self2 = this;
+                self2.callSuper('initialize', options);
+                self2.bSetSizes();
+            },
+            _render: function(ctx){
+                var self2 = this;
+                self2.callSuper('_render', ctx);
+                self2.bSetSizes();
+            }
+        });
+    };
+    BFabricPolygon.prototype.createBRect = function(){
+        var self = this;
+        var brect = new self.BRect({
+            fill:'#ff0000',
+            originX:'left',
+            originY:'top'
+        });
+        self.canvas.add(brect);
+        self.canvas.renderAll();
+    };
     BFabricPolygon.prototype.drawPolygon = function(){
         var self = this;
         self.polygonMode = true;
         self.pointArray = new Array();
         self.lineArray = new Array();
         self.activeLine = null;
+        self.notify("Start drawing polygon","success");
     };
     BFabricPolygon.prototype.addPoint = function(event){
         var self = this;
@@ -66,6 +136,7 @@ var BFabricPolygon = (function(){
         var id = new Date().getTime() + random;
         //Start creating the point in circle fabric object==============
         var pointer = self.canvas.getPointer( event.e );
+//        console.log(pointer.x.toString()+' '+pointer.y.toString());
         var circle = new fabric.Circle({
             radius: self.circlePointRadius, 
             fill: '#ffffff', 
@@ -87,8 +158,7 @@ var BFabricPolygon = (function(){
         }
         //Begin creating the line start at current point==============
         var points = [ pointer.x, pointer.y, pointer.x, pointer.y];
-        var line = new fabric.Line(points, {
-            strokeWidth: self.strokeWidthLine,
+        var line = new self.BLine(points, {
             fill: '#999999',
             stroke: '#999999',
             class:'line',
@@ -145,6 +215,25 @@ var BFabricPolygon = (function(){
         self.canvas.add( line );
         self.canvas.selection = false;
     };
+    
+    BFabricPolygon.prototype.notify = function( str, msg_type, selector ){
+        if(!jQuery){
+            return;
+        }
+        if( jQuery().notify ){
+            var is_msg_type = typeof msg_type !== 'undefined' ? true : false;
+            var is_selector = typeof selector !== 'undefined' ? true : false;
+            
+            if( is_msg_type && is_selector){
+                jQuery(selector).notify(str, msg_type);
+            }else if(is_msg_type){
+                jQuery.notify(str, msg_type);
+            }else if(is_selector){
+                jQuery(selector).notify(str);
+            }
+        }
+    };
+    
     BFabricPolygon.prototype.generatePolygon = function( p_pointArray ){
         var self = this;
 
